a2318151f08c63345fd3bb94cd06fc14c433bcb7
JiratchayaPremsil <jiratchaya5923@gmail.com>
2024-03-26 15:16:43 +0700

    update register

--- src/app/api/auth/register/page.tsx
+++ src/app/api/auth/register/page.tsx
@@ -2,7 +2,7 @@
 import { useState } from 'react';
 import userLogin from '@/libs/userLogIn';
 import userRegister from '@/libs/userRegister';
-
+import Image from 'next/image';
 
 export default function Register() {
     const [reg, setreg] = useState({
@@ -16,6 +16,7 @@ export default function Register() {
         setreg({ ...reg, [e.target.name]: e.target.value });
     };
 
+    
     const userRegis = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         await userRegister(reg.name, reg.tel, reg.email, reg.password);
@@ -23,30 +24,31 @@ export default function Register() {
     };
 
     return (
-        <main className="bg-slate-100 m-5 p-5">
+        <main className="bg-white m-5 p-5 rounded-xl shadow-md w-2/3 mx-auto">
         <form onSubmit={userRegis} className="flex justify-center">
-            <table className="w-1/2">
+            <table className="w-1/3">
                 <tbody>
-                    <tr className='pb-3'>
-                        <td className="text-right pr-4"><label htmlFor="name" className="text-gray-700">Name</label></td>
+                    <tr className='h-10'>
+                        <td className="text-left pr-4"><label htmlFor="name" className="text-black font-serif">Name: </label></td>
                         <td><input type="text" required id="name" name="name" placeholder="Name" className="input-field" onChange={handleChange} /></td>
                     </tr>
-                    <tr>
-                        <td className="text-right pr-4"><label htmlFor="tel" className="text-gray-700">Tel</label></td>
+                    <tr className='h-10'>
+                        <td className="text-left pr-4"><label htmlFor="tel" className="text-black font-serif">Tel: </label></td>
                         <td><input type="text" required id="tel" name="tel" placeholder="Tel" className="input-field" onChange={handleChange} /></td>
                     </tr>
-                    <tr>
-                        <td className="text-right pr-4"><label htmlFor="email" className="text-gray-700">Email</label></td>
+                    <tr className='h-10'>
+                        <td className="text-left pr-4"><label htmlFor="email" className="text-black font-serif">Email: </label></td>
                         <td><input type="text" required id="email" name="email" placeholder="Email" className="input-field" onChange={handleChange} /></td>
                     </tr>
-                    <tr>
-                        <td className="text-right pr-4"><label htmlFor="password" className="text-gray-700">Password</label></td>
+                    <tr className='h-10'>
+                        <td className="text-left pr-4"><label htmlFor="password" className="text-black font-serif">Password:</label></td>
                         <td><input type="password" required id="password" name="password" placeholder="Password" className="input-field" onChange={handleChange} /></td>
                     </tr>
                 </tbody>
             </table>
-            <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white p-2 rounded">Register</button>
+            <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white p-2 rounded-xl">Register</button>
         </form>
+        <Image src="/img/smileFace.png" width="400" height="300" alt='smiling face' className='mx-auto rounded-xl mt-10'></Image>
     </main>
     );
 }
